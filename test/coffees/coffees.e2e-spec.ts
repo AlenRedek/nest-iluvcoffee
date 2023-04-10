f13c4b996d3c15ca65e3593f078504fe9aedb0f7
Alen Redek <alen@redek.me>
2023-04-10 23:27:50 +0200

    Added additional testing database with scripts to automatically start / stop the DB

--- test/coffees/coffees.e2e-spec.ts
+++ test/coffees/coffees.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 
@@ -8,7 +9,19 @@ describe('[Feature] Coffees - /coffees', () => {
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoffeesModule],
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'pass123',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
